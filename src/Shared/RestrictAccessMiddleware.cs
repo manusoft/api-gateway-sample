e4f9adbc0a0a3e1ea66ea1d69d88f11b9bb6c99c
manoj@office <alconsoft@gmail.com>
2024-07-23 10:27:51 +0400

    Add shared project and add restrict access middleware.

---
+++ src/Shared/RestrictAccessMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Shared;
+
+public class RestrictAccessMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var referrer = context.Request.Headers["Referrer"].FirstOrDefault();
+
+        if (string.IsNullOrEmpty(referrer))
+        {
+            context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            await context.Response.WriteAsync("Hmmm, Can't reach this page.");
+            return;
+        }
+        else
+        {
+            await next(context);
+        }
+    }
+}
